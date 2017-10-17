adb25168f986d7b3f86996443cb948aeba5cd094
Deyan Vladkov <deian.vladkov@gmail.com>
2017-10-17 21:41:25 +0100

    grunt build

--- src/js/babel/twitch.js
+++ src/js/babel/twitch.js
@@ -1,15 +1,15 @@
-"use strict";
+'use strict';
+// ---------------------------------------------------------
+//    Be Aware - it's not real-time API for live streams !
+// ---------------------------------------------------------
 
-// -----------------------------------------------------------------------------------------
-// TODO: Flexbox Cards
-// Be Aware - it's not real-time API for live streams !
-// -----------------------------------------------------------------------------------------
 // Settings
+
 var channelUrl = "https://wind-bow.gomix.me/twitch-api/channels/";
 var streamUrl = "https://wind-bow.gomix.me/twitch-api/streams/";
+var proxy = "https://cors-anywhere.herokuapp.com/";
 var isStreamingLive = "";
 // Yup it's hardcoded
-//var channels = ["freecodecamp", "ESL_SC2", "OgamingSC2"];
 var channels = ["freecodecamp", "ESL_SC2", "OgamingSC2", "cretetion", "habathcx", "RobotCaleb", "noobs2ninjas", "comster404", "brunofin"];
 // Ugly multiple api hits
 var _iteratorNormalCompletion = true;
@@ -21,8 +21,7 @@ try {
         var channel = _step.value;
 
         var promiseGetChannel = new Promise(function (resolve, reject) {
-            //  console.log('channel ok');                
-            resolve(fetch(channelUrl + channel).then(function (response) {
+            resolve(fetch(proxy + channelUrl + channel).then(function (response) {
                 return response.json();
             }).then(function (json) {
                 return json;
@@ -30,8 +29,7 @@ try {
             reject(Error("Error: fetch channel from API failed"));
         });
         var promiseGetStream = new Promise(function (resolve, reject) {
-            //console.log('stream ok');
-            resolve(fetch(streamUrl + channel).then(function (response) {
+            resolve(fetch(proxy + streamUrl + channel).then(function (response) {
                 return response.json();
             }).then(function (json) {
                 return json;
@@ -39,22 +37,15 @@ try {
             reject(Error("Error: fetch stream from API failed"));
         });
         Promise.all([promiseGetChannel, promiseGetStream]).then(function (data) {
-            //console.log('Rendering data');
-            //console.log(data[0]);
-            //console.log(data[1]);
             if (data[0].hasOwnProperty("error")) {
-                //console.log(data[0].message)
                 data[0].display_name = data[0].message;
                 data[0].logo = "http://placehold.it/64x64";
                 data[0].url = "#";
             }
-            //console.log(data[0].display_name, data[0].logo
-            //console.log(data[0].display_name, data[0].logo, data[0].url);
             if (data[1].stream === null) {
-                // console.log('not streaming');
+                // Not streaming
                 isStreamingLive = "";
             } else {
-                //  console.log(data[1].stream.channel.status);
                 isStreamingLive = data[1].stream.channel.status;
             }
             $(".content").append(renderResult(data[0].display_name, data[0].logo, data[0].url, isStreamingLive));
